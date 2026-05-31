7e568faa20b6b11bc03f29e228771347d7f101d8
Anthony Oliveira Carvalho <anthonyol733@gmail.com>
2026-05-31 20:16:18 -0300

    Versão 1.1 do projeto

--- ColetaDeOleo.c
+++ ColetaDeOleo.c
@@ -21,24 +21,53 @@ void DateTimeCadaster(char *buffer, size_t sizeMax) {
 
 void ClearBuffer() {
     int trash;
-    while ((trash = getchar()) != '\n' && trash != EOF)
-        ;
+    while ((trash = getchar()) != '\n' && trash != EOF);
 }
 
-/*void TotalRaised(int assistant) {
-    assistant = 0;
-    if()
-}*/
+int searchDonor(Cadaster *cad, char search[]) {
+
+    FILE *cadasterFile = fopen("CadastroDoador.txt", "r");
+
+    if (cadasterFile == NULL) {
+        return 0;
+    }
+
+    char line[200];
+
+    while (fgets(line, sizeof(line), cadasterFile)) {
+
+        if (strncmp(line, "Nome: ", 6) == 0) {
+            strcpy(cad->name, line + 6);
+            cad->name[strcspn(cad->name, "\n")] = '\0';
+        }
+
+        else if (strncmp(line, "CPF: ", 5) == 0) {
+            strcpy(cad->cpf, line + 5);
+            cad->cpf[strcspn(cad->cpf, "\n")] = '\0';
+
+            if (strcmp(search, cad->name) == 0 ||
+                strcmp(search, cad->cpf) == 0) {
+
+                fclose(cadasterFile);
+                return 1;
+            }
+        }
+    }
+
+    fclose(cadasterFile);
+    return 0;
+}
 
 int main() {
 
-    FILE *arquivo;
-    arquivo = fopen("CadastroDoador.txt", "a");
+    FILE *cadasterFile, *registerFile, *reportFile, *totalFile;
+    cadasterFile = fopen("CadastroDoador.txt", "a");
+    registerFile = fopen("RegistroDoacao.txt", "a");
     Cadaster cad;
     Register reg;
-    int opcao = 0;
+    int option = 0;
 
-    while (opcao < 1 || opcao > 5) {
+    while (option < 1 || option > 5) {
         printf("===== SISTEMA DE DOACAO DE OLEO =====\n");
         printf("1 - Cadastrar doador.\n");
         printf("2 - Registrar doacao.\n");
@@ -47,16 +76,15 @@ int main() {
         printf("5 - Sair... .\n");
         printf("Digite a opcao desejada: ");
 
-        scanf("%d", &opcao);
+        scanf("%d", &option);
         ClearBuffer();
     }
-    fclose(arquivo);
+    fclose(cadasterFile);
 
-    switch (opcao) {
+    switch (option) {
     case 1:
-        arquivo = fopen("CadastroDoador.txt", "a+");
-        if (arquivo == NULL)
-        {
+        cadasterFile = fopen("CadastroDoador.txt", "a+");
+        if (cadasterFile == NULL){
             printf("Erro ao abrir o arquivo.\n");
             return 1;
         }
@@ -74,37 +102,57 @@ int main() {
         fgets(cad.phone, 20, stdin);
         cad.phone[strcspn(cad.phone, "\n")] = '\0';
 
-        fprintf(arquivo, "Nome: %s\n"
+        fprintf(cadasterFile, "Nome: %s\n"
                          "CPF: %s\n"
                          "Telefone: %s\n",
                 cad.name, cad.cpf, cad.phone);
-        char data_atual[20];
-        DateTimeCadaster(data_atual, sizeof(data_atual));
-        fprintf(arquivo, "Momento do cadastro: %s\n\n", data_atual);
-        fclose(arquivo);
+        char currentDate[20];
+        DateTimeCadaster(currentDate, sizeof(currentDate));
+        fprintf(cadasterFile, "Momento do cadastro: %s\n\n", currentDate);
+        fclose(cadasterFile);
         printf("Cadastro realizado com sucesso!\n");
         break;
 
     case 2:
-        arquivo = fopen("RegistroDoacao.txt", "a+");
-        printf("\nRegistro de doacao!\n");
-        printf("Quantidade de garrafas: ");
+        char search[100]; float totalPerUser = 0.0;
+        registerFile = fopen("RegistroDoacao.txt", "a+");
+        printf("\nRegistro de doacao:\n");
+        printf("Buscar doador (Nome ou CPF): ");
+        fgets(search, 100, stdin);
+        search[strcspn(search, "\n")] = '\0';
+        if(!searchDonor(&cad, search)) {
+            printf("Doador nao encontrado. Por favor, cadastre-se primeiro.\n");
+            break;
+        }
+        printf("Doador encontrado!\n");
+
+        printf("Quantidade de garrafas doadas: ");
         scanf("%u", &reg.quantity);
-
-        printf("Digite a quantidade de litros: ");
+        ClearBuffer();
+        printf("Informe a capacidade da garrafa (em litros): ");
         scanf("%f", &reg.liters);
-
-        fclose(arquivo);
-
+        ClearBuffer();
+        totalPerUser = reg.liters * reg.quantity;
+        printf("Total doado neste registro: %.2f litros\n", totalPerUser);
+        fprintf(registerFile, "Doador: %s\n"
+                         "Quantidade de garrafas: %u\n"
+                         "Capacidade da(s) garrafa(s) doada(s): %.2f litros\n"
+                         "Total doado neste registro: %.2f litros\n",
+                        cad.name, reg.quantity, reg.liters, totalPerUser);
+        
+        DateTimeCadaster(currentDate, sizeof(currentDate));
+        fprintf(registerFile, "Doação realizada em: %s\n\n", currentDate);
+        fclose(registerFile);
         break;
 
     case 3:
-        arquivo = fopen("relatorio.txt", "r+");
+        reportFile = fopen("relatorio.txt", "r");
+        reportFile = registerFile;
         printf("\nRelatorios!\n");
         break;
 
     case 4:
-        arquivo = fopen("registro.txt", "r+");
+        totalFile = fopen("registro.txt", "r+");
         printf("\nTotal arrecadado:\n");
         break;
 
@@ -112,5 +160,6 @@ int main() {
         printf("\nSaindo do sistema...\n");
         return 1;
     }
+
     return 0;
 }
